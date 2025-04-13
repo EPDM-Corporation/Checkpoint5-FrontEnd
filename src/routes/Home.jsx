09ebc0e38e505026602a99eb6776e5a243167c09
Erik Kaiyu Suguiyama <eriksug4@gmail.com>
2025-04-13 18:23:41 -0300

    Criando o design e os botões dos albuns na home

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -4,14 +4,26 @@ import './Home.css';
 
 const Home = () => {
   return (
-    <div className="home-container">
-        <div className='background-1'>
-            <div> Mais de 100 milhões de músicas,
+    <div className="home-container flex items-center">
+            <div className='green-text lg:w-200 m-20 text-3xl'> Mais de 100 milhões de músicas,
                  álbuns e podcasts esperando por você.
                   Do hit do momento ao som que só você 
                   conhece tudo num só play.</div>
-            
-        </div>
+            <div className='flex items-center'>
+                <div className='album-button'>
+                    <div className='left-arrow'></div>
+                </div>
+                <div className='album-bg'>
+
+                </div>
+                <div className='album-button'>
+                    <div className='right-arrow'></div>
+                </div>
+            </div>
+
+            <div className='home-disk-bg w-full h-100 m-5'>
+
+            </div>
     </div>
   );
 };
