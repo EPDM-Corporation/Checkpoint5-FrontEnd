9d51d556de1ff1afa7876769094c2a60d478b706
Erik Kaiyu Suguiyama <eriksug4@gmail.com>
2025-04-13 10:28:03 -0300

    Adicionando o footer e o header nos components

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,22 @@ import Home from './routes/Home'
 import Sobre from './routes/Sobre'
 import Depoimento from './routes/Depoimento'
 import Error from './routes/Error'
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
 
 function App() {
 
   return (
+    
     <BrowserRouter>
+      <Header/>
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='*' element={<Error/>}/>
         <Route path='/sobre' element={<Sobre/>}/>
         <Route path='/depoimento' element={<Depoimento/>}/>
       </Routes>
+      <Footer/>
     </BrowserRouter>
   )
 }
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Header.css'
+import {Link} from 'react-router-dom'
+
+const Header = () => {
+  return (
+    <div>
+      <h1>Header</h1>
+    </div>
+  )
+}
+
+export default Header
