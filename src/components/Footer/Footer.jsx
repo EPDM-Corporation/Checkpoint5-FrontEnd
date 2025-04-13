2a616663792a9094a0129c3b807cfb44ed1a7cff
Erik Kaiyu Suguiyama <eriksug4@gmail.com>
2025-04-13 11:04:54 -0300

    Instalando e testando tailwind

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import './Footer.css'
-
 const Footer = () => {
   return (
-    <div>
-      <h1>Footer</h1>
-    </div>
+    <footer className='bg-gray-900 max-w h-39'>
+        <div>
+            <div className='text-white'>Contatos</div>
+            <div className='text-white'>Email: melodia@gmail.com</div>
+            <div className='text-white'>Telefone:(11) 98765-4321</div>
+        </div>
+        
+    </footer>
   )
 }
 
