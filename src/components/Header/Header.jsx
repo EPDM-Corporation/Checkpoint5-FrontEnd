8f3e8467f67e8b6d211d662f76089d861c37632f
Erik Kaiyu Suguiyama <eriksug4@gmail.com>
2025-04-14 08:11:04 -0300

    Criando o header fixo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,34 +1,46 @@
-import React, { useState } from 'react';
-import './Header.css';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import './Header.css';
 
 export default function Header() {
   const [isOpen, setIsOpen] = useState(false);
-  return (
-    <header className="header flex flex-col justify-center items-center">
-    <div>
-      <div className="header-logo flex justify-center">
-        <Link to="/">🔥 Melodia</Link>
-      </div>
+  const [scrolled, setScrolled] = useState(false);
 
-      <nav className="header-menu">
-        <Link to="/" className='hidden sm:block'>Home</Link>
-        <Link to="/sobre" className='hidden sm:block'>Sobre</Link>
-        <Link to="/depoimento" className='hidden sm:block'>Depoimentos</Link>
-        <Link to="/contato" className='hidden sm:block'>Contato</Link>
-      </nav>
-    </div>
-    <button id="menu-btn" className="sm:hidden text-3xl" onClick={() => setIsOpen(!isOpen)} >
+  useEffect(() => {
+    const handleScroll = () => {
+      const isScrolled = window.scrollY > 50;
+      setScrolled(isScrolled);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
+  return (
+    <header className={`header flex flex-col justify-center items-center ${scrolled ? 'scrolled' : ''}`}>
+      <div>
+        <div className="header-logo flex justify-center">
+          <Link to="/">🔥 Melodia</Link>
+        </div>
+
+        <nav className={`header-menu ${scrolled ? 'scrolled' : ''}`}>
+          <Link to="/" className='hidden sm:block'>Home</Link>
+          <Link to="/sobre" className='hidden sm:block'>Sobre</Link>
+          <Link to="/depoimento" className='hidden sm:block'>Depoimentos</Link>
+          <Link to="/contato" className='hidden sm:block'>Contato</Link>
+        </nav>
+      </div>
+      <button id="menu-btn" className={`sm:hidden text-3xl mobile-button ${scrolled ? 'scrolled' : ''}`} onClick={() => setIsOpen(!isOpen)} >
         ☰
-    </button>
-        {isOpen && (
-            <div className='flex flex-col items-center block sm:hidden'>
-               <Link to="/" onClick={() => setIsOpen(false)}>Home</Link>
-               <Link to="/sobre" onClick={() => setIsOpen(false)}>Sobre</Link>
-               <Link to="/depoimento" onClick={() => setIsOpen(false)}>Depoimento</Link>
-               <Link to="/contato" onClick={() => setIsOpen(false)}>Contato</Link>
-            </div>
-        )}
+      </button>
+      {isOpen && (
+        <div className='flex flex-col items-center block sm:hidden'>
+          <Link to="/" onClick={() => setIsOpen(false)}>Home</Link>
+          <Link to="/sobre" onClick={() => setIsOpen(false)}>Sobre</Link>
+          <Link to="/depoimento" onClick={() => setIsOpen(false)}>Depoimento</Link>
+          <Link to="/contato" onClick={() => setIsOpen(false)}>Contato</Link>
+        </div>
+      )}
     </header>
   );
 }
