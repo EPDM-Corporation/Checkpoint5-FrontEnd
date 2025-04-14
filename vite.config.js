c810e8e678b234e2abaa1caa58f985bfcac83300
Erik Kaiyu Suguiyama <eriksug4@gmail.com>
2025-04-14 10:41:20 -0300

    Asdalsdjals

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/Checkpoint5-FrontEnd/",
+  base: "./",
   plugins: [react(),
   tailwindcss()
   ],
